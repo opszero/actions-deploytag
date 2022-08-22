44002285d72f941775b28c4033102dde26e958a3
Abhi Yerra <abhi@berkeley.edu>
2022-08-22 10:55:55 -0700

    use env

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const githubRef = core.getInput('github-ref').replace(/^refs\/heads\//, '');
-  const previewEnvName = githubRef.replace(/[^A-Za-z0-9]/g, '-')
+  const previewEnvName = process.env.GITHUB_REF_NAME.replace(/[^A-Za-z0-9]/g, '-')
   console.log(`Preview Env Name ${previewEnvName}!`);
 
   const isPreview = !!githubRef.match(/^(bug|epic|feature_deploy)/)
