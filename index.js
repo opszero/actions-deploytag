03f0217a5a88b91f021499cf7f895ce7dedc8ef7
Abhi Yerra <abhi@berkeley.edu>
2022-08-22 11:13:38 -0700

    Fix semicolons

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const githubRef = process.env.GITHUB_HEAD_REF
-  console.log(`Github Ref: ${githubRef}`)
-  const previewEnvName = githubRef.replace(/[^A-Za-z0-9]/g, '-')
+  const githubRef = process.env.GITHUB_HEAD_REF;
+  console.log(`Github Ref: ${githubRef}`);
+  const previewEnvName = githubRef.replace(/[^A-Za-z0-9]/g, '-').toLowerCase();
   console.log(`Preview Env Name ${previewEnvName}!`);
 
-  const isPreview = !!githubRef.match(/^(bug|epic|feature_deploy)/)
+  const isPreview = !!githubRef.match(/^(bug|epic|feature_deploy)/);
   console.log(`Is Preview: ${isPreview}!`);
 
   core.setOutput("preview-env-name", previewEnvName);
